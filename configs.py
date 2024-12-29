b1d2b941c45124c12dab90ce0667cd11d1f76500
mezoro-code <sologameacg@gmail.com>
2024-12-29 10:12:40 +0600

    Update configs.py

--- configs.py
+++ configs.py
@@ -32,12 +32,10 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 ╰──────[ 😎 ]───────────⍟
 """
   ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [zoro](https://t.me/+rOX1pEcNmIkyYmQ1)
  
  I am Super noob Please Support My Hard Work.
 
-[Donate Me](https://t.me/KingVj01)
-"""
   HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
 
